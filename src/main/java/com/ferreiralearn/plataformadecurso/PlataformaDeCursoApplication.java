0da9b116070efeecabad129832635462b7de0cde
Andrey Cadmo <49537908+AndreyCadmo@users.noreply.github.com>
2025-10-23 21:11:33 -0300

    create: app and add dependencies

---
+++ src/main/java/com/ferreiralearn/plataformadecurso/PlataformaDeCursoApplication.java
@@ -0,0 +1,13 @@
+package com.ferreiralearn.plataformadecurso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlataformaDeCursoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlataformaDeCursoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ferreiralearn/plataformadecurso/PlataformaDeCursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ferreiralearn.plataformadecurso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlataformaDeCursoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
